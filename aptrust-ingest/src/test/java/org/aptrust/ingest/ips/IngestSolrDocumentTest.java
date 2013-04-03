419689da82baa8034656b15801852a1c90afd0d4
Michael Durbin <md5wz@md5wz.lib.virginia.edu>
2013-04-03 14:06:50 -0400

    Updated solr schema to enable full-text searching and added a test.

--- aptrust-ingest/src/test/java/org/aptrust/ingest/ips/IngestSolrDocumentTest.java
+++ aptrust-ingest/src/test/java/org/aptrust/ingest/ips/IngestSolrDocumentTest.java
@@ -1,11 +1,14 @@
 package org.aptrust.ingest.ips;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
+
+import junit.framework.Assert;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrServer;
@@ -15,7 +18,9 @@ import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.core.CoreContainer;
 import org.aptrust.common.solr.AptrustSolrDocument;
+import org.aptrust.ingest.api.DigitalObject;
 import org.aptrust.ingest.api.IngestManifest;
+import org.aptrust.ingest.api.IngestPackage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -57,24 +62,38 @@ public class IngestSolrDocumentTest {
         IngestManifest m = (IngestManifest) unmarshaller.unmarshal(this.getClass().getClassLoader().getResourceAsStream("manifest-fedora-sample.xml"));
 
         m.setId("test:1");
-        
+
         IngestSolrDocument ingest = IngestSolrDocument.newIngest("test", m);
         SolrInputDocument doc = AptrustSolrDocument.createValidSolrDocument(ingest);
-        
+
         server.add(doc);
         server.commit();
-        
+
+        for (IngestPackage p : m.getPackagesToSubmit()) {
+            PackageSolrDocument pDoc = new PackageSolrDocument(p);
+            server.add(AptrustSolrDocument.createValidSolrDocument(pDoc));
+        }
+        server.commit();
+
         ModifiableSolrParams params = new ModifiableSolrParams();
         params.set("q", AptrustSolrDocument.RECORD_TYPE + ":ingest");
         SolrDocumentList results = server.query(params).getResults();
-        assertEquals(results.getNumFound(), (long) 1);
+        Assert.assertEquals("There must be exactly one ingest record in solr (found " + results.getNumFound() + ").", (long) 1, results.getNumFound());
         
         params.set("q", AptrustSolrDocument.INCLUDED_PID + ":\"uva-lib:602138\"");
         results = server.query(params).getResults();
-        assertEquals(results.getNumFound(), (long) 1);
+        assertEquals("Expected to find a single record for uva-lib:602138.", (long) 1, results.getNumFound());
+        
+        params.set("q", AptrustSolrDocument.RECORD_TYPE + ":package");
+        results = server.query(params).getResults();
+        assertEquals("Expected to find 3 package records but found " + results.getNumFound() + ".", (long) 3, results.getNumFound());
         
         params.set("q", AptrustSolrDocument.INCLUDED_PID + ":\"test:fake\"");
         results = server.query(params).getResults();
-        assertEquals(results.getNumFound(), (long) 0);
+        assertEquals("Expected not to find fake record.", (long) 0, results.getNumFound());
+        
+        params.set("q", "Novvelles");
+        results = server.query(params).getResults();
+        assertEquals("Expected to find one result for the full-text search for \"Novvelles\" but found " + results.getNumFound()  + ".", (long) 1, results.getNumFound());
     }
 }
